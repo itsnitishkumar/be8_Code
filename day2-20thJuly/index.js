a0503f08d676999be2b21a092b3ace9682c20c41
Nitish <nk301998@gmail.com>
2022-07-21 17:49:32 +0530

    deleted index file from base directory, and code pushed for the date 20th july

---
+++ day2-20thJuly/index.js
@@ -0,0 +1,38 @@
+name = 'Nitish'
+age=24.5
+male=true
+// abc
+arr=[1,'Nitish',3]
+obj = {
+    name: 'Nitish',
+    age: 24
+}
+// console.log(obj)
+// console.log(typeof obj)
+
+
+// -------------------------------
+
+// console.log(1+1)
+// console.log(1+'1')
+
+
+
+// -----------------------------
+
+// console.log('a');
+// console.log(Number('a'));
+// console.log(typeof Number('a'));
+
+// console.log(1==1)
+// console.log(1=='1')
+// console.log(1===1)
+// console.log(1==='1')
+
+
+let a=1;
+let b=1;
+console.log(a==b);
+let a1=1;
+let b1='1';
+console.log(a==b);
