185e75b89842a1b81ab2567c7c15191a1666623c
Nitish <nk301998@gmail.com>
2022-07-22 16:45:12 +0530

    examples of pre and post inc/dec on 21st july

---
+++ day3-21stJuly/index.js
@@ -0,0 +1,18 @@
+//pre inc/dec 
+//post inc/dec
+
+a=1
+console.log(a+1)
+console.log(a+2 + a++) 
+console.log(a++ + ++a + a++ + ++a + ++a)
+++a 
+console.log(a+b); 
+console.log(a); 
+console.log(a--); 
+console.log(--a);
+console.log(++a);
+
+console.log(2*(a++) - 5*(--a));
+// -------------------------------
+
+
