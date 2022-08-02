7a5e3bda5b8760e9a3ed20eb1c5a1b49099d0c19
Nitish <nk301998@gmail.com>
2022-08-02 22:01:24 +0530

    changes commited

---
+++ day5-23rdJuly/index.js
@@ -0,0 +1,22 @@
+console.log(1+1);
+console.log(1-1);
+console.log(1+'1');
+console.log(1-'1');
+console.log(1+null);
+console.log(1-null);
+console.log(1+undefined);
+console.log(1+true);
+console.log(1+false);
+console.log(1-undefined);
+console.log(typeof null)
+
+console.log(true);
+console.log(true+false);
+
+console.log(1+'undefined');
+console.log(undefined+1);
+console.log(1+undefined);
+console.log(null+undefined);
+console.log(typeof(1 + ''));
+
+console.log();
